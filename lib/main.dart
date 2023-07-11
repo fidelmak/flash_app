dd7192cf2a755f63be095e7a5551189c9e2325fc
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2023-07-11 06:00:57 +0100

    Please enter the commit message for your changes. Lines starting

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flash_app/screens/chat_screen.dart';
+import 'package:flash_app/screens/login_screen.dart';
+import 'package:flash_app/screens/registration_screen.dart';
+import 'package:flash_app/screens/welcome_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+// ...
+
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Product UIs',
+      theme: ThemeData(
+        // backgroundColor: const Color.fromARGB(255, 43, 41, 41),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      initialRoute: WelcomeScreen.ID,
+      //home: product_one(),
+      routes: {
+        WelcomeScreen.ID: (context) => WelcomeScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+
+        //'/home2': ( context) => home(),
+
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        ChatScreen.id: (context) => ChatScreen(),
+      },
+      //product_one(),
+    );
+  }
+}
---
+++ lib/screens/registration_screen.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import 'chat_screen.dart';
+
+class RegistrationScreen extends StatefulWidget {
+  static String id = "register";
+  @override
+  _RegistrationScreenState createState() => _RegistrationScreenState();
+}
+
+class _RegistrationScreenState extends State<RegistrationScreen> {
+  final _auth = FirebaseAuth.instance;
+  late String email;
+  late String password;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Hero(
+              tag: 'logo',
+              child: Container(
+                height: 200.0,
+                child: Image.asset('images/logo.png'),
+              ),
+            ),
+            SizedBox(
+              height: 48.0,
+            ),
+            TextField(
+                keyboardType: TextInputType.emailAddress,
+                textAlign: TextAlign.center,
+                onChanged: (value) {
+                  //Do something with the user input.
+                  email = value;
+                },
+                decoration: kDecoration("input your email")),
+            SizedBox(
+              height: 8.0,
+            ),
+            TextField(
+              obscureText: true,
+              textAlign: TextAlign.center,
+              onChanged: (value) {
+                //Do something with the user input.
+                password = value;
+              },
+              decoration: kDecoration("input your password"),
+            ),
+            SizedBox(
+              height: 24.0,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 16.0),
+              child: Material(
+                color: Colors.blueAccent,
+                borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                elevation: 5.0,
+                child: MaterialButton(
+                  onPressed: () async {
+                    try {
+                      final newUser =
+                          await _auth.createUserWithEmailAndPassword(
+                              email: email, password: password);
+                      if (newUser != null) {
+                        Navigator.pushNamed(context, ChatScreen.id);
+                      }
+                    } catch (e) {
+                      print(e);
+                    }
+                    //Implement registration functionality.
+                    // print(email);
+                    // print(password);
+                  },
+                  minWidth: 200.0,
+                  height: 42.0,
+                  child: Text(
+                    'Register',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  InputDecoration kDecoration(String hint) {
+    return InputDecoration(
+      hintText: hint,
+      contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.all(Radius.circular(32.0)),
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: Colors.blueAccent, width: 1.0),
+        borderRadius: BorderRadius.all(Radius.circular(32.0)),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: Colors.blueAccent, width: 2.0),
+        borderRadius: BorderRadius.all(Radius.circular(32.0)),
+      ),
+    );
+  }
+}
